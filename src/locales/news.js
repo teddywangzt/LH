fe5186364f157f047a05fe8284139d9eb006866e
teddywangzt <53000462+teddywangzt@users.noreply.github.com>
2019-11-18 17:55:37 +0800

    Update news.js

--- src/locales/news.js
+++ src/locales/news.js
@@ -60,21 +60,183 @@ window.newsData = {
   list: [
      {
       cate: "PORTFOLIO",
+      title: "零氪荣获“2019年大数据产业十大应用案例”",
+      link: "https://mp.weixin.qq.com/s/zaZ0n80FzqgmxYkwlsg6hA",
+      source: "LinkDoc视界",
+      abstract:
+        "11月10日，海淀区委宣传部、《数据》杂志在中关村国家自主创新示范区会议中心共同举办“2019大数据产业十大应用案例发布会”，零氪科技凭借在医疗人工智能领域的探索，荣膺“2019年大数据产业十大应用案例”，是医疗大数据领域唯一的获奖企业代表。",
+      date: "November 15, 2019"
+   },      
+    {
+      cate: "LONG HILL CAPITAL",
+      title: "长岭新闻 | 长岭资本参加第32届AVCJ私募股权与创业投资亚洲论坛",
+      link: "https://mp.weixin.qq.com/s/fR3OhK3W1xyY7THqL-2i5Q",
+      source: "长岭资本",
+      abstract:
+        "2019年11月12日，第32届“AVCJ私募股权与创业投资亚洲论坛”在香港四季酒店举办。AVCJ是亚洲最大且最有影响力的私募股权会议，此次参会总人数超过1150人。长岭资本管理合伙人蒋晓冬先生受邀作为论坛嘉宾参与了名为“VC and beyond: The changing deal environment”的圆桌论坛。",
+      date: "November 13, 2019"
+    },                 
+    {
+      cate: "PORTFOLIO",
+      title: "“用数据使生命更美好，促进社会服务运行能力和效率” 海淀区委书 记一行莅临零氪科技调研",
+      link: "https://mp.weixin.qq.com/s/UTxfkhA0_fQPgNpFpctc_w",
+      source: "LinkDoc视界",
+      abstract:
+        "近日，北京市海淀区委书记于军，北京市委党校机关党委专职副书记、机关纪委书记王春虹，北京市海淀区委常委、副区长李俊杰等一行深入医疗大数据及人工智能（AI）企业零氪科技，开展“不忘初心、牢记使命，我在基层促发展”主题调研活动，了解企业发展情况，征求意见建议，为科技创新“主力军”提供支持和帮助。",
+      date: "November 8, 2019"
+    }, 
+      {
+      cate: "PORTFOLIO",
+      title: "MORE Health爱医传递接受日本东久迩宫国际文化褒赏",
+      link: "https://www.aiyichuandi.com/gsdt/61917653.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "2019年11月3日，知名全球数字医疗服务机构MORE Health爱医传递接受了日本东久迩宫国际文化褒赏，得到日本天皇家族的认可，获得日本天皇勋章及奖状。MORE Health爱医传递是本次褒奖仪式上的唯一一个中国获奖者，这是中日交流历史上的大事。",
+      date: "November 4, 2019"
+    }, 
+     {
+      cate: "PORTFOLIO",
+      title: "新起点 · 至未来！东方启音12周年庆典精彩回顾！",
+      link: "https://mp.weixin.qq.com/s/QKfXbREEJuC-vG28Whs_rQ",
+      source: "东方启音",
+      abstract:
+        "“东方启音将以科技赋能，线上线下联动，开展融合教育支持，让千万家庭受益。”2019年11月3日，东方启音12周年庆“新起点 至未来”在深圳举行，东方启音创始人、总裁姚秋武作出上述表示。数据显示，成立12年来，致力于儿童言语与智能发展的专业机构东方启音，已在国内18个城市和地区设立了30余家直营中心，累计服务所需家庭超过20万。无论是治疗效果还是发展规模，均已稳坐言语治疗行业的头把交椅。",
+      date: "November 3, 2019"
+    },                 
+     {
+      cate: "PORTFOLIO",
+      title: "BCEIA 2019圆满落幕 ！泉心泉意收获颇丰！",
+      link: "https://mp.weixin.qq.com/s/OfHSxWZT7wJqd0etY5JiUw",
+      source: "泉心泉意Lab Direct",
+      abstract:
+        "“东方启音将以科技赋能，线上线下联动，开展融合教育支持，让千万家庭受益。”2019年11月3日，东方启音12周年庆“新起点 至未来”在深圳举行，东方启音创始人、总裁姚秋武作出上述表示。数据显示，成立12年来，致力于儿童言语与智能发展的专业机构东方启音，已在国内18个城市和地区设立了30余家直营中心，累计服务所需家庭超过20万。无论是治疗效果还是发展规模，均已稳坐言语治疗行业的头把交椅。",
+      date: "November 3, 2019"
+    },   
+        {
+      cate: "LONG HILL CAPITAL",
+      title: "长岭新闻 | 高歌百岁人生，纵情光辉岁月 — 长岭资本迎来成立三周年",
+      link: "https://mp.weixin.qq.com/s/bU3DLa4tgcb6V8Hyq37TSA",
+      source: "长岭资本",
+      abstract:
+        "2019年10月31日，以“光辉岁月”为主题的长岭资本三周年庆典，暨2019年基金合伙人大会欢迎晚宴在上海静安瑞吉酒店成功举办。",
+      date: "November 1, 2019"
+    },
+    {
+      cate: "PORTFOLIO",
+      title: "海吉亚医疗拟赴港IPO 联合创始人“中国伽马刀之父”",
+      link: "https://finance.sina.com.cn/stock/hkstock/ggipo/2019-10-31/doc-iicezuev6137657.shtml",
+      source: "新浪财经",
+      abstract:
+        "资料显示，海吉亚医疗是一家中美合资专业化的医疗投资集团，专注于连锁肿瘤治疗及脑科医疗机构的投资、建设及运营管理，朱义文为该集团董事长，总裁为季海荣，其联合创始人为宋世鹏与朱义文。",
+      date: "October 31, 2019"
+    },     
+     {
+      cate: "PORTFOLIO",
+      title: "MORE Health爱医传递携手锦州医大三院，共建中美远程会诊中心",
+      link: "https://www.aiyichuandi.com/gsdt/55212505.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "2019年10月30日上午，锦州医科大学附属第三医院举行了互联网远程会诊中心启动仪式。会上，MORE Health爱医传递与锦州医大三院正式签订战略合作协议。未来，双方共同搭建的中美远程会诊平台将助力锦州地区医疗卫生事业蓬勃发展，让患者足不出户就能与美国专家“面对面”进行沟通和问诊。",
+      date: "October 31, 2019"
+    }, 
+     {
+      cate: "PORTFOLIO",
+      title: "颈医卫助力“彭博一英里接力赛”圆满举行",
+      link: "https://mp.weixin.qq.com/s/fvvN1GcqQEI7Y-pPyh82OA",
+      source: "颈医卫",
+      abstract:
+        "由全球知名的财经资讯品牌彭博社举办的“彭博一英里接力赛”——上海站，于昨日开跑，颈医卫受邀作为该赛事的支持单位，助力活动圆满成功。颈医卫作为沪上知名健康品牌，亦十分重视本次活动，从内部选拔多名优秀医护人员参与其中。近5小时的活动中，颈医卫医护人员快速有效的干预处理，让参赛选手能够顺利完成比赛，收获选手们一致好评。",
+      date: "October 26, 2019"
+    },   
+     {
+      cate: "PORTFOLIO",
+      title: "北京，免费的心理咨询，你来不来？",
+      link: "https://mp.weixin.qq.com/s/4oK4O-FLuT5RhCip8hvWOw",
+      source: "简单心理",
+      abstract:
+        "目前，北京市卫生健康委员会正在开展“北京市居民心理健康筛查及心理问题干预”项目，为北京市居民提供心理测评、心理热线和免费心理咨询服务，关怀居民心理健康。简单心理入选了该项目社会心理服务机构，现在正在为符合条件的北京居民提供面对面心理咨询，每人可最多享5次免费咨询机会。",
+      date: "October 23, 2019"
+    },
+     {
+      cate: "PORTFOLIO",
+      title: "国际医疗合作高峰论坛在京隆重召开，USCIPP访华团全员出席共话中美医疗合作",
+      link: "https://www.aiyichuandi.com/gsdt/69053677.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "2019年10月18日，由知名全球跨境医疗服务机构MORE Health爱医传递和在国际医学领域赫赫有名的美国医院国际医疗协会（USCIPP）联合主办的国际医疗合作高峰论坛在北京隆重举行。代表团覆盖US News年度最佳医院排行榜总排名前三的梅奥诊所、麻省总医院和约翰霍普金斯医院，以及在国际上享有盛誉的纪念斯隆-凯特林癌症中心、加州大学旧金山分校医疗中心、克利夫兰诊所、希望之城国家医疗中心、哈佛大学医学院附属布莱根妇女医院等全美18家医疗机构。",
+      date: "October 21, 2019"
+    },  
+      {
+      cate: "PORTFOLIO",
+      title: "世界互联网大会发布“全球独角兽榜” 零氪成国内唯一医疗大数据上榜企业",
+      link: "https://mp.weixin.qq.com/s/taB1FVcDaPt2zYEVrESMBw",
+      source: "LinkDoc视界",
+      abstract:
+        "第六届世界互联网大会10月20日开幕，全球目光再次齐聚浙江乌镇。国内权威商业研究机构-胡润研究院21日重磅发布首份《2019胡润全球独角兽榜》（Hurun Global Unicorn List 2019）。零氪科技以70亿人民币估值跻身榜单，成为医疗大数据赛道上唯一上榜的国内企业。",
+      date: "October 21, 2019"
+    },   
+     {
+      cate: "PORTFOLIO",
+      title: "2019固生堂苏州区域膏方节开幕暨苏州大儒分院一周年庆",
+      link: "https://mp.weixin.qq.com/s/8DTkECaSkIWkcAS-ig3tSQ",
+      source: "固生堂中医",
+      abstract:
+        "10月12日清晨，坐标固生堂苏州区域（固生堂苏州桃花坞分院和固生堂苏州大儒分院），筹划了近一个月的膏方节正式开幕。此外，膏方节现场云集了很多行业大咖，如来自上海中医药大学附属龙华医院的特邀专家王菊勇教授等。同时，固生堂苏州大儒分院迎来了一周年店庆，苏州区域小伙伴上下一心，不怕艰苦，排除万难，使得固生堂在苏州地区迅速形成了良好的口碑及城市影响力。",
+      date: "October 14, 2019"
+    },   
+     {
+      cate: "PORTFOLIO",
+      title: "爱医传递携手日本天皇贴身御医，为您打造“天皇同款”医疗服务",
+      link: "https://www.aiyichuandi.com/gsdt/92339941.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "2019年10月11日，MORE Health爱医传递携手日本医疗科技公司Integrity Healthcare成立合资公司International Healthcare，强强联合，打造最专业的中日就医平台，为中国患者量身定制全方位医疗服务。",
+      date: "October 14, 2019"
+    },     
+     {
+      cate: "PORTFOLIO",
+      title: "全球入选率＜3%！零氪科技登榜“2019全球数字医疗”中国独角兽三甲",
+      link: "https://mp.weixin.qq.com/s/olHBR8R_-eDLLUlp_uHSog",
+      source: "Linkdoc视界",
+      abstract:
+        "近日，CB Insights公布了今年最新的数字医疗150强榜单，评选出全球最有前景的150家数字医疗初创公司，覆盖医药供应、保险福利、基因组学、社区医疗、临床实验等15个细分领域，零氪科技入选榜单。零氪科技还以超10亿美金的估值名列榜单中17家“独角兽”之一，列席中国数字医疗“独角兽”三甲。",
+      date: "October 8, 2019"
+    },      
+     {
+      cate: "PORTFOLIO",
+      title: "贺电！MORE Health爱医传递入选全球150家最具前景数字医疗初创公司",
+      link: "https://www.aiyichuandi.com/gsdt/29048027.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "第九届中美健康峰会于2019年9月26-27号在哈佛医学院举办。此次峰会更是汇聚了中美医疗领域近200位权威学者，包括医疗保健领域、医疗卫生体系专家50多人，中国一线三甲医院院长40多人以及知名医生上百人。MORE Health爱医传递作为本届中美健康峰会唯一受邀跨境医疗企业参加此次盛会。",
+      date: "October 4, 2019"
+    }, 
+     {
+      cate: "PORTFOLIO",
+      title: "百位中美医疗健康届领袖齐聚第九届中美健康峰会 ——MORE Health爱医传递受邀出席",
+      link: "https://www.aiyichuandi.com/gsdt/95444346.html",
+      source: "MORE Health爱医传递",
+      abstract:
+        "2019年10月2日，CB Insights发布了全球150家最有前景的数字医疗初创公司，MORE Health爱医传递名列其中，尤其在临床工具（clinical tool）板块中名列第一，被誉为数字医疗公司中的佼佼者，正在重新定义着医疗产业。",
+      date: "October 1, 2019"
+    },     
+     {
+      cate: "PORTFOLIO",
       title: "MORE Health爱医传递协助举办四大医学院波士顿校友会",
       link: "https://mp.weixin.qq.com/s/_uADqdMT0ATEzLypWOoCVw",
       source: "MORE Health爱医传递",
       abstract:
         "2019年8月31日下午，由南京大学、中国药科大学、东南大学以及沈阳药科大学波士顿校友联合主办、全球数字医疗服务机构MORE Health爱医传递协办的波士顿（新英格兰）地区校友会于马萨诸塞州布鲁克莱恩（Brookline）的拉兹安德森公园（Larz Anderson Park）成功举行。",
-      date: "September 29,2019"
-    },       
-     {
+      date: "September 29, 2019"
+    },          
+    {
       cate: "PORTFOLIO",
       title: "全球首个治疗系统性红斑狼疮生物制剂“倍力腾” 在中国获批 邻客DTP药房助力广州妇儿医疗中心完成首销",
       link: "https://mp.weixin.qq.com/s/xBOwPJYoMSYImmrsCd5xgQ",
       source: "LinkDoc视界",
       abstract:
         "9月26日，经过广州市妇女儿童医疗中心医学伦理委员会及药事管理部门批准，该中心过敏免疫风湿病科率先使用倍力腾®（注射用贝利尤单抗）。广州南外大药房-邻客控股顺利完成倍力腾在广州市妇女儿童医疗中心的首销，成功配送至妇儿中心风湿免疫科患者手中，并成功实现输注。未来，倍力腾®将为广大系统性红斑狼疮患者提供全新的生物靶向治疗方案！",
-      date: "September 26,2019"
+      date: "September 26, 2019"
     },       
      {
       cate: "PORTFOLIO",
@@ -83,7 +245,7 @@ window.newsData = {
       source: "MORE Health爱医传递",
       abstract:
         "2019年第61届美国放射肿瘤学会（ASTRO）年会于2019年9月15日至18日在美国伊利诺伊州芝加哥举行。ASTRO会议第二天，山东省肿瘤医院与MORE Health爱医传递在ASTRO大会期间正式签署了“互联网+国际远程会诊”战略合作协议。",
-      date: "September 25,2019"
+      date: "September 25, 2019"
     },       
      {
       cate: "PORTFOLIO",
@@ -92,7 +254,7 @@ window.newsData = {
       source: "LinkDoc视界",
       abstract:
         "“我国肿瘤患者每年总花费约为4000亿元，每年新增患者大约400万，误诊、漏诊率高达40%。”9月22日，在第四届中国医药创新与投资大会上，零氪科技CEO张天泽用一组触目惊心的数据阐明攻克癌症并非易事，而在他看来，大数据与人工智能（AI）将成为攻克癌症的有力武器。会上，张天泽从医药行业应用切入，以大数据与AI技术推动下肿瘤创新药物研发突破为视角，为行业提供了新的视角和实践经验。",
-      date: "September 24,2019"
+      date: "September 24, 2019"
     },   
     {
       cate: "PORTFOLIO",
@@ -101,7 +263,7 @@ window.newsData = {
       source: "固生堂中医",
       abstract:
         "9月14日晚，由广东广播电视台、南方财经全媒体集团、广东省工商业联合会联合主办，广东广播电视台经济频道制作承办的南中国最高端的企业家访谈电视节目《风云粤商》播出，固生堂中医连锁管理集团董事长兼总裁涂志亮受邀参与，为观众做了以“三年融资十七亿 传统中医如何抢占新赛道？”为主题的分享，讲述他与他的团队共同奋斗的故事。",
-      date: "September 23,2019"
+      date: "September 23, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -110,7 +272,7 @@ window.newsData = {
       source: "LinkDoc视界",
       abstract:
         "9月19日，2019中国临床肿瘤学（CSCO）年会在厦门召开，零氪科技主办的以“零航数智，氪话未来”为主题的的卫星会上，中国医科大学附属第一医院肿瘤内科主任刘云鹏教授联合零氪科技首次发布了“化疗后骨髓抑制风险预测AI”；复旦大学附属中山医院胸部肿瘤和呼吸介入科副主任胡洁教授，介绍了如何利用多组学数据，研发AI用于肺癌早筛查。",
-      date: "September 20,2019"
+      date: "September 20, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -119,7 +281,7 @@ window.newsData = {
       source: "泉心泉意Lab Direct",
       abstract:
         "方达控股（方达医药上市主体）于2019年5月30日在港交所上市；位于上海张江生物医药基地的近4000㎡生物分析实验室，也于2019年9月宣布正式运营，方达最终选择与泉心泉意“整体实验室搬迁”团队建立合作。搬迁又适逢方达医药接受国家核查组核查，为保证核查组到来前完成搬迁及调试任务，泉心泉意增派专业服务工程师和专业搬迁技术工人，在保证服务质量前提下，将原本2周左右的搬迁项目时间压缩到了1周，赢得了客户高度赞扬。",
-      date: "September 19,2019"
+      date: "September 19, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -128,7 +290,7 @@ window.newsData = {
       source: "固生堂中医",
       abstract:
         "9月6日~12日，2019年国际(亳州)中医药博览会暨第35届全国(亳州)中药材交易会在安徽亳州盛大召开。固生堂董事长兼总裁涂志亮先生此次不仅受邀出席高峰论坛，更与在场大咖们围绕“中医馆如何品牌化经营、互联网中医如何开展”等话题展开了深刻的交流和讨论。凭借在中医药行业所做出的杰出贡献，以及为中医药文化的传承付出的心血，涂志亮先生于本届药博会上荣获“全国中医馆功勋人物奖”，成为固生堂发展历程中里程碑事件。",
-      date: "September 19,2019"
+      date: "September 19, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -137,7 +299,7 @@ window.newsData = {
       source: "德州新闻网",
       abstract:
         "9月6日，德州海吉亚医院建设项目签约仪式在康博酒店举行。海吉亚医疗集团总裁季海容，副市长范宇新，区领导沈四平、宋世忠、韩冰出席。仪式上，季海容与区党工委副书记、管委会副主任、政法委书记宋世忠共同签署合作协议。据了解，该项目总投资10亿元，总规划建筑面积11万平方米，计划设置床位1000张。",
-      date: "September 13,2019"
+      date: "September 13, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -146,7 +308,7 @@ window.newsData = {
       source: "固生堂中医",
       abstract:
         "9月7日上午，为期一天的首届全国中医馆医事药事发展高峰论坛在亳州拉开帷幕。这也是2019年国际（亳州）中医药博览会暨第35届全国（亳州）中药材交易会的第一项活动。固生堂中医连锁管理集团董事长兼总裁涂志亮在本次医高峰论坛中获颁“全国中医馆功勋人物奖”，副总裁严峻代表上台领奖。",
-      date: "September 9,2019"
+      date: "September 9, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -155,7 +317,7 @@ window.newsData = {
       source: "LinkDoc世界",
       abstract:
         "2019年9月5日，由中国商务部、中国贸促会和宁夏回族自治区人民政府共同举办的第四届中国-阿拉伯国家博览会在银川国际会展中心正式拉开帷幕，中阿携手建设开放型世界经济，共建“一带一路”，推动构建人类命运共同体。零氪科技作为医疗大数据与人工智能企业代表，参与“互联网+医疗健康”示范区主题展示。",
-      date: "September 6,2019"
+      date: "September 6, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -164,7 +326,7 @@ window.newsData = {
       source: "LinkDoc世界",
       abstract:
         "“为什么是零氪科技？”。9月4日，在《E药经理人》微解药平台播出的“中国医药行业意见领袖访谈录独角兽系列-零氪科技专场”上，主持人康复之家集团创始人柏煜问道。成立于2014年的零氪，真正切入医药行业的时间并不长，但凭借在肿瘤领域的技术能力，以大数据赋能药品全生命周期管理、助推药企转型升级，正显露出独特的竞争优势。零氪科技联合创始人兼首席临床运营官李丽平给出了自己的解答：从解决行业问题出发，打造数据闭环，做全链路解决方案。",
-      date: "September 5,2019"
+      date: "September 5, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -173,7 +335,7 @@ window.newsData = {
       source: "固生堂中医",
       abstract:
         "日前，2019年粤商•省长面对面协商座谈会在广州召开。会上，13位粤商代表围绕主题，就推动技术创新、知识产权保护、高端人才培养、营商环境优化、检测认证市场化、质量监管体系建设等问题提出意见建议。其中，来自医药服务领域的省政协委员、固生堂中医连锁管理集团董事长兼总裁涂志亮建议，探索建立高品质中药材标准，避免“劣药驱逐良药”。",
-      date: "September 5,2019"
+      date: "September 5, 2019"
     },
     {
       cate: "PORTFOLIO",
@@ -623,144 +785,6 @@ window.newsData = {
       abstract:
         "2019年4月30日，跨境医疗机构MORE Health爱医传递与位于美国俄亥俄州哥伦布的全国儿童医院(Nationwide Children's Hospital)正式签订战略合作协议，这一强强联手为中国患者创造了更加优质便捷的问诊途径，患儿能够通过MORE Health的线上医师协作平台(Physician Collaboration Platform™)，享受全国儿童医院的诊疗资源。",
       date: "May 05, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "东阿阿胶与固生堂宣布达成战略合作",
-      link: "https://www.gstzy.cn/news/list/7/11504.html",
-      source: "固生堂中医",
-      abstract:
-        "4月26日，长岭资本在A轮独家投资企业-固生堂中医连锁管理集团与东阿阿胶股份有限公司宣布达成战略合作。当天，东阿阿胶股份有限公司总裁秦玉峰、助理总裁田维、市场总监胡志庆等，固生堂董事长涂志亮、运营副总监谭文飞、广州中医药大学原校长徐志伟等出席战略合作签约仪式，签约仪式由东阿阿胶副总裁田维主持。",
-      date: "April 28, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "“以患者为先，以服务为重”，杰特贝林与邻客达成全国战略合作",
-      link: "https://mp.weixin.qq.com/s/7IkwoOvDrfGGDODxzz02Ew",
-      source: "Linkdoc视界",
-      abstract:
-        "4月24日，杰特贝林产品咨询部总监陈巍先生、全国DTP经理奚悦女士一行莅临零氪科技参观交流，并与零氪线下布局——邻客·智慧药房达成全国战略合作，未来其产品“人血白蛋白”将覆盖全国范围内邻客·智慧药房，使更多患者从中受益。",
-      date: "April 25, 2019"
-    },
-
-    {
-      cate: "PORTFOLIO",
-      title: "波士顿儿童医院和MORE Health宣布进行战略合作",
-      link: "https://www.aiyichuandi.com/news/5cc026ba834d691500d3e3ae",
-      source: "MORE Health 爱医传递",
-      abstract:
-        "美通社加州圣马特奥2019年4月23日 儿科医疗中心波士顿儿童医院(Boston Children's Hospital)和全球医疗公司MORE Health爱医传递宣布建立战略联盟，让中国病人能够通过MORE Health的医师协作平台(Physician Collaboration Platform™)，获得波士顿儿童医院的医师的诊疗。",
-      date: "April 24, 2019"
-    },
-    {
-      cate: "LONG HILL CAPITAL",
-      title: "长岭资本参加第八届中国医疗健康产业投资与并购CEO峰会",
-      link: "https://mp.weixin.qq.com/s/gDj8ChtTtUEjaAeArrtLwA",
-      source: "长岭资本",
-      abstract:
-        "2019年4月10日-4月12日由CHC中国医疗咨询集团携手中信证券联合主办的“第八届中国医疗健康产业投资与并购CEO峰会”在上海东郊宾馆举行。长岭资本管理合伙人蒋晓冬先生受邀作为论坛嘉宾参与了名为“医疗企业的孵化投资及早期投资”的圆桌论坛。",
-      date: "April 23, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title:
-        "权威发布：邻客•智慧药房入榜 “2018-2019年度中国药店价值榜潜力100强”",
-      link: "https://mp.weixin.qq.com/s/7oZuVE7AzOmSRU6dZuCCjg",
-      source: "Linkdoc视界",
-      abstract:
-        "4月17-19日，2018-2019年度中国药店发展报告暨价值榜发布会在天津盛大举办，会上，中国药店杂志社隆重揭晓了“2018-2019年度中国药店价值榜潜力100强”榜单，邻客·智慧药房以第32名的排名成功入榜，并成为DTP行业唯一入榜的企业。",
-      date: "April 19, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "单县海吉亚医院与单县蔡堂镇中心卫生院医联体启动仪式圆满成功",
-      link: "http://www.shanxianhaijiya.com.cn/news_show.aspx?id=673",
-      source: "单县海吉亚医院",
-      abstract:
-        "2019年4月15日上午，单县海吉亚医院与单县蔡堂镇中心卫生院医联体启动仪式在单县蔡堂镇中心卫生院举行。单县海吉亚医院院长王更轩、护理部主任黄克霞一行以及单县蔡堂镇中心卫生院院长李林光等领导班子共计100余人参加了医联体启动仪式。",
-      date: "April 16, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "颈医卫第五家门店——新昌店开始试营业",
-      link: "http://www.neckyw.com/a/jingyiwei/c3/142.html",
-      source: "颈医卫",
-      abstract:
-        "4月11日，经过紧锣密鼓的筹备，颈医卫第五家门店——新昌店开始试营业啦！新昌店坐落于黄埔区新昌路，紧靠1号线新闸路地铁站，交通十分便利。兼具科技感与时尚感的大厅设计，浸入式体验的产品展示区，简约时尚、轻松活跃的诊室，带给大家更好的颈腰养护体验。",
-      date: "April 12, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "邻客·智慧药房与爱医传递达成战略合作 无缝链接重疾患者诊疗全流程",
-      link: "https://mp.weixin.qq.com/s/lKrkrTcwhcqj9jbC9gSJMQ",
-      source: "Linkdoc视界",
-      abstract:
-        "日前，邻客·智慧药房与爱医传递正式签订战略合作协议，双方将依托国际化的远程会诊平台，一方面基于爱医传递来自中美优质的医生资源，为中国重症患者提供权威、精准、高效的中美会诊及第二诊疗方案服务；另一方面，邻客·智慧药房将通过MDT多学科会诊中心，将优质的医疗资源，先进的治疗理念，多品类、最前沿的新特药产品，下沉到患者。",
-      date: "April 10, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "零氪科技荣获“2019年度医疗新技术最佳投资案例奖”",
-      link: "https://vcbeat.net/MDgyMDZjYmE0ZGRkYWJlNTlmYzRjYjJmM2QyY2EyMDA=",
-      source: "动脉网",
-      abstract:
-        "2019年4月10日，由浩悦资本主办、主题为“破局”的“第三届医疗健康投资卓悦榜峰会”在上海锦江汤臣洲际大酒店成功举办。其间“2019第三届医疗健康投资卓悦榜”榜单也正式发布，现场超过400名投资人和业内权威嘉宾济济一堂，零氪科技荣获“2019年度医疗新技术最佳投资案例奖”。",
-      date: "April 10, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "便民的不仅有饿了么、美团、滴滴，还有邻客+爱医",
-      link: "https://mp.weixin.qq.com/s/4ZdbxETS3Nnl1MNvbk6RzA",
-      source: "MORE Health 爱医传递",
-      abstract:
-        "日前，爱医传递与邻客·智慧药房正式签订战略合作协议。依托国际化的远程会诊平台，爱医传递汇聚中美优质的医生资源，为中国重症患者提供权威、精准、高效的中美会诊及第二诊疗方案服务；邻客·智慧药房将基于远程会诊的开具的电子处方，为患者提供多品类、最前沿的新特药产品，科学管理患者的院外第三生存空间。",
-      date: "April 10, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "1个月签约近30家，我们一起再爆发！",
-      link: "https://mp.weixin.qq.com/s/36sfsGvjVDu7GKYpLYHdUA",
-      source: "CiCiLight新鲜里",
-      abstract:
-        "上个月CICILIGHT新鲜里正式开放全国加盟后，立即接到了来自全国各地的咨询电话和上门洽谈，截至今天新鲜里一共已经完成近30家加盟店的签约，所在地区分别为上海，河北，江苏，浙江，湖北，四川，辽宁，山东，河南，贵州........ 各地门店，将陆续启动。",
-      date: "April 09, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "涂志亮：追梦路上 选对平台抓住资源成就自我",
-      link: "https://mp.weixin.qq.com/s/QDqpXdcpJ_kCQWitH-wWaQ",
-      source: "固生堂中医",
-      abstract:
-        "4月3日晚，董事长涂志亮开通直播频道，线上为500多名小固分享了他与固生堂的点滴故事：“奋斗者是很孤独的，不要在意别人怎么看你，要遵循自己内心的想法，坚持自己的梦想和远方，选择以后，请坚持并坚定。因为方向对了，行业对了，平台对了，即使会有很多不如意，但我们都在调整和进步，让我们一起前进，互相成就，越来越好。”",
-      date: "April 08, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "MORE Health爱医传递 与 希望之城癌症中心 达成战略合作",
-      link: "https://www.aiyichuandi.com/news/5ca4185a90c34f16009b2836",
-      source: "MORE Health 爱医传递",
-      abstract:
-        "全球互联网医疗机构MORE Health爱医传递今日正式宣布，与美国希望之城(City of Hope)国家医疗中心签订战略合作协议。希望之城国际医学中心副主任Edwin McCarthy表示：“MORE Health爱医传递已经做好准备，为国际患者提供先进的癌症咨询服务，使他们成为我们在美国以外拓展专业知识和医疗服务的重要合作伙伴。”",
-      date: "April 03, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title: "“万人筛查·千人授课”自闭症早期干预公益活动总结发布会在京召开",
-      link: "http://www.dfqy.com/news/detail/258",
-      source: "东方启音",
-      abstract:
-        "“万人筛查·千人授课”自闭症(“孤独症”)早期干预公益活动2018-2019年度总结发布会于4月2日在北京举行。中国人口福利基金会携手东方启音及众多公益伙伴，回顾了过去一年中该公益项目的运营情况，东方启音集团总裁姚秋武对“该项目进行了积极部署和规划。",
-      date: "April 03, 2019"
-    },
-    {
-      cate: "PORTFOLIO",
-      title:
-        "邻客·智慧药房与乐康肿瘤专家医生集团启动战略合作 助力DTP药房3.0时代",
-      link: "https://mp.weixin.qq.com/s/32Soq7qvV4ihFbUstoqnVg",
-      source: "Linkdoc视界",
-      abstract:
-        "3月31日，邻客·智慧药房与广东乐康肿瘤专家医生集团签署战略合作协议，开创了DTP药房与医生集团合作的新模式。双方将在零氪科技医疗大数据赋能下，依托互联网医院平台，为线上线下的患者提供权威、精准、高效的远程问诊、名医会诊、手术转诊及患者教育等服务，以期为患者建立优质的院外“第三生存空间”。",
-      date: "April 01, 2019"
     },
     {
       cate: "LONG HILL CAPITAL",
